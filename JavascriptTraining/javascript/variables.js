d27cd95d47f4220813081ffafa3b60709690ca28
Onur Nigiz <onurnigiz@hotmail.com>
2022-10-01 15:44:22 +0300

    variables training

---
+++ JavascriptTraining/javascript/variables.js
@@ -0,0 +1,31 @@
+// var ile değişken tanımlama
+// var serverName = "api.kodluyoruz.org"
+// console.log(serverName)
+
+
+// let ile değişkeni boş tanımlamak
+let serverName; 
+console.log(serverName)
+// let ile değişkene bilgi atamak
+serverName = "api.kodluyoruz.org"
+console.log(serverName)
+
+// let ile değişkene bilgi atayarak tanimlamak
+let password = "12345"
+console.log(password)
+
+// değişken ataması yapmadan önce kullanmaya çalışmak:
+// var kullanılırsa undefined çevirir
+// let ile kullanılırsa hata koduyla ve açıklamasıyla console üzerinde görülür
+
+// let ile tanimlanan değişkenin içindeki bilgiyi değiştirebilirsin, üzerine yazabilirsin
+// birleştirme ve ekleme işlemi de yapılabilir
+// serverName = serverName + "lorem ipsum dolor"
+
+
+// const ile değişkeni bos tanimlamaya calismak :/
+// degistirelemeyen variable
+// değişken atayarak tanımlamak gerekiyor
+// degistirmeye calisirsak hata verir
+
+const serverPassword = "1224235234"
